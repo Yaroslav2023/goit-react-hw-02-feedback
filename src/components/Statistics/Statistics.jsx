5dc7dd91fc0593d3022ab4415f985e7d21e01556
Yaroslav2023 <yishevchenko@ukr.net>
2023-08-01 22:12:54 +0300

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,7 +8,7 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
       <p>Total: {total}</p>
-      <p>Positive feedback: {positivePercentage}</p>
+      <p>Positive feedback: {positivePercentage}%</p>
     </div>
   );
 };
